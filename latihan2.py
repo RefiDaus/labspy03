039ff86a931f3e687a78733b8d9e3d3d46973221
RefiDaus <refirefi12@gmail.com>
2021-11-07 19:45:24 +0700

    menambahkan file pyton

---
+++ latihan2.py
@@ -0,0 +1,14 @@
+print ("PROGRAM PUTHON MENGHITUNG NILAI TERKECIL & TERBESAR SERTA RATA-RATA")
+n = int(input("\nmasukan jumlah bilangan = "))
+bil = []
+tot = 0
+for x in range(n):
+    m=x+1
+    a = int(input("bilangan ke %d = "%m))
+    bil.append (a)
+    tot += bil [x]
+    rata2 = tot / n
+
+print("\nbilangan terkecil : %d" %min(bil))
+print("bilangan terbesar : %d" %max(bil))
+print("nilai rata-rata : %0.2f" %rata2)
